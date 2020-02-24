78259ef3f67f9d5535017a5a7cc65405d769f369
onjectiondevops <45457762+onjectiondevops@users.noreply.github.com>
2020-02-24 16:39:28 +0530

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -1,10 +1,10 @@
 const { events, Job } = require("brigadier");
 events.on("push", () => {
-  var job = new Job("test-job", "alpine:3.8");
+  var job = new Job("test-job", "bitnami/kubectl");
   job.tasks = [
     "echo Hello",
     "echo World",
-    "ls -lhtr ; pwd"
+    "get pods"
   ];
   job.run();
 });
